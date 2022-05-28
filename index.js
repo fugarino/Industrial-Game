236557f514c0697d9eca2c857fe9f76110a05c1e
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-28 13:48:34 -0400

    platforms

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ canvas.height = 576;
 // Gravity will accelerate player's falling speed
 const gravity = 0.5;
 
+// Images
+const platformVines = new Image();
+platformVines.src = "./img/Industrial-Platform-Vines.png";
+
 // Player class will store and update all properties of player (new Player)
 class Player {
   constructor() {
@@ -23,8 +27,8 @@ class Player {
       y: 0,
     };
     // width and height determine the size of the player
-    this.width = 88;
-    this.height = 88;
+    this.width = 50;
+    this.height = 50;
   }
   // the draw method is drawing a red square refrencing the position and size of the player
   draw() {
@@ -43,7 +47,29 @@ class Player {
   }
 }
 
+class Platform {
+  constructor({ x, y, image }) {
+    this.width = 336;
+    this.height = 56;
+    this.position = {
+      x: x,
+      y: y,
+    };
+    this.image = image;
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+
 const player = new Player();
+const platforms = [
+  new Platform({ x: 0, y: 500, image: platformVines }),
+  new Platform({ x: 322, y: 500, image: platformVines }),
+  new Platform({ x: 644, y: 500, image: platformVines }),
+  new Platform({ x: 966, y: 500, image: platformVines }),
+];
 
 const keys = {
   left: {
@@ -54,19 +80,43 @@ const keys = {
   },
 };
 
+// animate function will recursively clear the entire canvas and then update the player properties
 function animate() {
   // recursively calling animate function to create a game loop
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
   player.update();
 
   if (keys.right.pressed) {
-    player.velocity.x = 5;
+    if (player.position.x + player.width >= canvas.width) {
+      player.velocity.x = 0;
+    } else {
+      player.velocity.x = 5;
+    }
   } else if (keys.left.pressed) {
-    player.velocity.x = -5;
+    if (player.position.x <= 0) {
+      player.velocity.x = 0;
+    } else {
+      player.velocity.x = -5;
+    }
   } else {
     player.velocity.x = 0;
   }
+
+  // Platform collision detection
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >= platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
+    }
+  });
 }
 animate();
 
@@ -74,7 +124,7 @@ animate();
 window.addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "w": // Up
-      if (player.position.y + player.height - 2 === canvas.height) player.velocity.y = -20;
+      if (player.velocity.y === 0) player.velocity.y = -17;
       break;
     case "s": // Down
       break;
